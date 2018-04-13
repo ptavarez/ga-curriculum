b8671e631ea54b8caee5a8075964406d54e34c9b
ptavarez16 <ptavarez16@yahoo.com>
2018-04-13 06:27:58 -0400

    Deployment Steps

--- config/environment.js
+++ config/environment.js
@@ -3,10 +3,11 @@
 module.exports = function (environment) {
   'use strict';
   const ENV = {
-    modulePrefix: 'ga-wdi-boston.ember-auth',
+    modulePrefix: 'ga-curriculum',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    apiHost: 'http://localhost:4741/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -49,7 +50,9 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
+    ENV.rootURL = '/ga-curriculum';
     ENV.locationType = 'hash';
+    ENV.apiHost = 'https://ga-curriculum.herokuapp.com/';
   }
 
   return ENV;
